f20648344527bed4a1aaf847b1c2521e28a9537a
Annoying POTATO <36124755+pota2dev@users.noreply.github.com>
2022-03-23 17:38:16 +0600

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -31,8 +31,9 @@ from pyrogram import (
 
 buttons = [
     [
-        InlineKeyboardButton('⚡️Make Own Bot', url='https://github.com/subinps/VCPlayerBot'),
-        InlineKeyboardButton('🧩 Join Here', url='https://t.me/subin_works'),
+        #InlineKeyboardButton('⚡️Make Own Bot', url='https://github.com/subinps/VCPlayerBot'),
+        #InlineKeyboardButton('🧩 Join Here', url='https://t.me/subin_works'),
+        InlineKeyboardButton('🥔OWNER🥔', url='https://t.me/PotatoTheGreat'),
     ]
     ]
 def get_cmd(dur):
@@ -47,7 +48,7 @@ async def search(client, query):
         answers.append(
             InlineQueryResultArticle(
                 title="Deploy",
-                input_message_content=InputTextMessageContent(f"{Config.REPLY_MESSAGE}\n\n<b>You can't use this bot in your group, for that you have to make your own bot from the [SOURCE CODE](https://github.com/subinps/VCPlayerBot) below.</b>", disable_web_page_preview=True),
+                input_message_content=InputTextMessageContent(f"{Config.REPLY_MESSAGE}\n\n<b>I'm just a bot to play music. I don't have time to talk to you.</b>", disable_web_page_preview=True),
                 reply_markup=InlineKeyboardMarkup(buttons)
                 )
             )
