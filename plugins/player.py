5b6a8f8f5507b51e87656bddeae3e87d11dc0f37
Annoying POTATO <36124755+pota2dev@users.noreply.github.com>
2022-03-23 23:10:11 +0600

    Update player.py

--- plugins/player.py
+++ plugins/player.py
@@ -501,8 +501,7 @@ async def not_chat(_, m: Message):
     else:
         buttons = [
             [
-                InlineKeyboardButton('⚡️Make Own Bot', url='https://github.com/subinps/VCPlayerBot'),
-                InlineKeyboardButton('🧩 Join Here', url='https://t.me/subin_works'),
+                InlineKeyboardButton('🥔OWNER🥔', url='https://github.com/subinps/PotatoTheGreat'),
             ]
             ]
         await m.reply("<b>You can't use this bot in this group, for that you have to make your own bot from the [SOURCE CODE](https://github.com/subinps/VCPlayerBot) below.</b>", disable_web_page_preview=True, reply_markup=InlineKeyboardMarkup(buttons))
